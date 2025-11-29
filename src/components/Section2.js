21ddb5a8032df81eef17fd83b8975fe4826f9c9c
Just Noob Coder <140948892+nguyenngocthuan11062004@users.noreply.github.com>
2025-11-29 15:16:27 +0700

    item image effect

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,4 +1,6 @@
 import React from "react";
+import PixelTransition from "./PixelTransition";
+import "../assets/css/style.css";
 import product1 from "../assets/images/product-1.jpg";
 import product2 from "../assets/images/product-2.jpg";
 import product3 from "../assets/images/product-3.jpg";
@@ -20,13 +22,40 @@ function Section2() {
           {products.map((product) => (
             <div className="product-item" key={product.id}>
               <div className="inner-image">
-                <a href="#"><img src={product.image} alt={product.name} /></a>
+                <PixelTransition
+                  firstContent={
+                    <img
+                      src={product.image}
+                      alt={product.name}
+                      className="product-image"
+                    />
+                  }
+                  secondContent={
+                    <div className="pixel-second-content">
+                      <p className="pixel-second-name">
+                        {product.name}
+                      </p>
+                      <p className="pixel-second-price">
+                        {product.price} ₫
+                      </p>
+                      <span className="pixel-second-cta">
+                        Xem chi tiết
+                      </span>
+                    </div>
+                  }
+                  gridSize={7}
+                  pixelColor="#ffffff"
+                  once={false}
+                  animationStepDuration={0.4}
+                  className="product-pixel-card"
+                />
               </div>
+
               <div className="inner-content">
                 <h3 className="inner-title">
                   <a href="#">{product.name}</a>
                 </h3>
-                <div className="inner-price">{product.price}</div>
+                <div className="inner-price">{product.price} ₫</div>
               </div>
             </div>
           ))}
