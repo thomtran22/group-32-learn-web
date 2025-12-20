6879a7f0bb25ceabccbc6a6bcc3baf70ac921500
hongthom02022004@gmail.com <hongthom02022004@gamil.com>
2025-12-20 12:03:40 +0700

    Add to check

--- backend/models/CategoryModel.js
+++ backend/models/CategoryModel.js
@@ -16,7 +16,7 @@ const categorySchema = mongoose.Schema(
         parent: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Category', // Tham chiếu đến chính Model Category này
-            default: null,   // Nếu là null thì đây là danh mục cha lớn nhất (ví dụ: Áo Nam)
+            default: null,   // Nếu là null thì đây là danh mục cha lớn nhất
         }
     },
     //Tự động cập nhật thời gian tạo và thời gian cập nhật
